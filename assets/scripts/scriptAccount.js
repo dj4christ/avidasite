ab206b6fd222e9398def4432cb14c087de17cbf0
Deacon Griffiths <dj4christ09@gmail.com>
2025-10-12 16:10:20 +0200

    Add the profile page

---
+++ assets/scripts/scriptAccount.js
@@ -0,0 +1,18 @@
+const iframe = document.getElementById("options");
+
+function loadProfile() {
+    iframe.src = "../html/profile.html";
+}
+
+function loadCreator() {
+    iframe.src = "../html/creator.html";
+}
+
+function loadUpload() {
+    iframe.src = "../html/upload.html";
+}
+
+function loadContact() {
+    iframe.src = "../html/contact.html";
+}
+
---
+++ assets/scripts/scriptProfile.js
@@ -0,0 +1,12 @@
+const loginButton = document.getElementById("login");
+var loggedIn = true;
+
+loginButton.hidden = true;
+
+if (!loggedIn) {
+    loginButton.hidden = false;
+}
+
+function login() {
+    
+}
